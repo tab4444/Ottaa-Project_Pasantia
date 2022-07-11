b4a6eb1f73322710651f949080eee21336fb642a
tab4 <103967187+tab4444@users.noreply.github.com>
2022-07-11 12:47:00 -0300

    Add files via upload

--- Lautaro_Angel_Tiago.ino
+++ Lautaro_Angel_Tiago.ino
@@ -1,3 +1,4 @@
+//#include <Mouse.h>
 /* Arduino Mouse Emulation 
  *  learnelectronics
  *  based on an original sketch by   by: Jim Lindblom date: 1/12/2012
@@ -9,7 +10,6 @@
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
 //                                                                              LIBRARY
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
-#include <Mouse.h>
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
 //                                                                             VARIABLES
@@ -22,7 +22,7 @@ int vertZero, horzZero;                                                         
 int vertValue, horzValue;                                                                         // Stores current analog output of each axis
 const int sensitivity = 200;                                                                      // Higher sensitivity value = slower mouse, should be <= about 500
 int mouseClickFlag = 0;
-
+int valor = 0;
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
 //                                                                             SETUP
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -52,23 +52,49 @@ void loop()
   Serial.print("y ");
   Serial.println(vertValue);
  */
-                                                                                                  // Mouse.move(xpos,ypos,wheel)
-                                                                                                  // Mouse.move is always relative to the last position of the mouse cursor
+        /*valor = analogRead(A0);
+Serial.print("Valor analógico : ");
+Serial.println(valor);
+delay(1000);   */                                                                                         // Mouse.move(xpos,ypos,wheel)
+                                                                                             // Mouse.move is always relative to the last position of the mouse cursor
+  if(vertValue>=120){
+  Serial.print("Arriba");  
+  }
+                                                                      // Mouse.move is always relative to the last position of the mouse cursor
+  else if(vertValue<=120){
+  Serial.print("Abajo");  
+  }
+  else{
+    Serial.print("Quieto");
+  }
 
+  if(horzValue>=120){
+  Serial.print("Derecha");  
+  }
+                                                                      // Mouse.move is always relative to the last position of the mouse cursor
+  else if(horzValue<120){
+  Serial.print("Abajo");  
+  }
+  else{
+    Serial.print("Quieto");
+  }
   if (vertValue != 0)
-    Mouse.move(0, vertValue/sensitivity, 0);                                                      // move mouse on y axis
+    //Mouse.move(0, vertValue/sensitivity, 0);                                                      // move mouse on y axis
   if (horzValue != 0)
-    Mouse.move((horzValue/sensitivity)*-1, 0, 0);                                                 // move mouse on x axis
+   // Mouse.move((horzValue/sensitivity)*-1, 0, 0);                                                 // move mouse on x axis
 
     
   if ((digitalRead(selPin) == 0) && (!mouseClickFlag))                                            // if the joystick button is pressed
   {
     mouseClickFlag = 1;
-    Mouse.press(MOUSE_LEFT);                                                                      // click the left button down
+    //Mouse.press(MOUSE_LEFT);   
+
+    // click the left button down
   }
   else if ((digitalRead(selPin))&&(mouseClickFlag))                                               // if the joystick button is not pressed
   {
     mouseClickFlag = 0;
-    Mouse.release(MOUSE_LEFT);                                                                    // release the left button
+    //Mouse.release(MOUSE_LEFT);                                                                    // release the left button
+
   }
 }
